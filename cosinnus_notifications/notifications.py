5e53beffacd9868eecea5c2b664c950de0029cf8
Sascha Narr <saschanarr@googlemail.com>
2014-11-11 11:47:39 +0100

    Better configuration system for notifications

--- cosinnus_notifications/notifications.py
+++ cosinnus_notifications/notifications.py
@@ -5,27 +5,26 @@ from collections import defaultdict
 
 from cosinnus.core.registries.apps import app_registry
 from django.utils.importlib import import_module
-from cosinnus_notifications.mail import send_mail_or_fail
+from cosinnus.core.mail import get_common_mail_context, send_mail_or_fail
+from django.template.loader import render_to_string
+from cosinnus.utils.functions import ensure_dict_keys
 
 
 # the dict to store all configured notification signals and options
-notification_groups = defaultdict(list)
+notifications = {}
 
 def _find_notification(signal):
     """ Finds a configured notification for a received signal """
-    the_notification = None
-    for app, notifications in notification_groups.items():
-        for notification in notifications:
-            if signal in notification[3]:
-                the_notification = notification
-                break
-    return the_notification
+    for signal_id, options in notifications.items():
+        if signal in options['signals']:
+            return signal_id
+    return None
 
 def check_user_wants_notification(user, notification_id):
     ">>> dummy notification WANT check: returning True"
     return True
 
-def notification_receiver(sender, user, audience, **kwargs):
+def notification_receiver(sender, user, obj, audience, **kwargs):
     """ Generic receiver function for all notifications 
         sender: the main object that is being updated / created
         user: the user that modified the object and caused the event
@@ -33,20 +32,33 @@ def notification_receiver(sender, user, audience, **kwargs):
     """
     signal = kwargs['signal']
     # find out configured signal id for the signal we received
-    notification = _find_notification(signal)
-    if not notification:
+    notification_id = _find_notification(signal)
+    if not notification_id:
         return
+    options = notifications[notification_id]
     
-    print ">>> notify caught a signal with id", notification[0], "and audience", audience
-    for user in audience:
-        if check_user_wants_notification(user, notification[0]):
-            template = notification[2]
-            get_common_mail_context
-            send_mail_or_fail(user.email, subject, template, context)
+    print ">>> notify caught a signal with id", notification_id, "and audience", audience
+    for receiver in audience:
+        if check_user_wants_notification(receiver, notification_id):
+            template = options['mail_template']
+            subj_template = options['subject_template']
+            if sender.request:
+                context = get_common_mail_context(sender.request)
+            else:
+                print ">>> warn: no request in sender"
+                context = {}
+            
+            context.update({
+                'receiver': receiver,
+                'source_user': user,
+                'object': obj,
+            })
+            subject = render_to_string(subj_template, context)
+            send_mail_or_fail(receiver.email, subject, template, context)
 
 
 def init_notifications():
-    global notification_groups 
+    global notifications 
     
     print ">> init notifications"
     for app, app_name, app_label in app_registry.items():
@@ -55,16 +67,20 @@ def init_notifications():
             notification_module = import_module('%s.cosinnus_notifications' % app)
         except ImportError:
             continue
-        print ">> sigs:", notification_module.notification_groups
-        for signal_id, label, template, signals in notification_module.notification_groups:
-            signal_id = "%s:%s" % (app_name, signal_id)
-            print "connecting", signal_id
-            notification_groups[app_name].append((signal_id, label, template, signals))
-            # connect to signals
-            for signal in signals:
-                signal.connect(notification_receiver)
-            
-            
-            
-            
+        if hasattr(notification_module, 'notifications'):
+            print ">> sigs:", notification_module.notifications
+            for signal_id, options in notification_module.notifications.items():
+                #label, template, signals
+                signal_id = "%s:%s" % (app_name, signal_id)
+                ensure_dict_keys(options, ['label', 'mail_template', 'subject_template', 'signals'], \
+                    "The configured notification '%s' of app '%s' was missing the following dict keys: %%s" \
+                    % (signal_id, app_name))
+                
+                options['app_name'] = app_name
+                print "connecting", signal_id
+                notifications[signal_id] = options
+                # connect to signals
+                for signal in options['signals']:
+                    signal.connect(notification_receiver)
+                
             
