80a3b1224b1ba85eb9f318f12f82c0c2cd6556cc
Sascha Narr <saschanarr@googlemail.com>
2014-11-11 18:13:58 +0100

    Add model UserNotificationPreference, initial migrations and admin

--- cosinnus_notifications/admin.py
+++ cosinnus_notifications/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from cosinnus_notifications.models import UserNotificationPreference
 
 
+class UserNotificationPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'group', 'notification_id', 'is_active')
+    list_filter = ('group', 'user', 'is_active',)
+
+admin.site.register(UserNotificationPreference, UserNotificationPreferenceAdmin)
--- cosinnus_notifications/models.py
+++ cosinnus_notifications/models.py
@@ -1,14 +1,41 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import datetime
-
-from django.core.urlresolvers import reverse
-from django.db import models as django_models
+from django.db import models
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
-from django.utils.timezone import now
+from django.utils.encoding import python_2_unicode_compatible
 
+from cosinnus.conf import settings
+from cosinnus.models.group import CosinnusGroup
+
+@python_2_unicode_compatible
+class UserNotificationPreference(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+        verbose_name=_('Notification Preference'),
+        on_delete=models.CASCADE,
+        related_name='notifications'
+    )
+    group = models.ForeignKey(CosinnusGroup, related_name='user_notification_preferences',
+        on_delete=models.CASCADE)
+    notification_id = models.CharField(_('Notification ID'), max_length=100)
+    is_active = models.BooleanField(default=0)
     
+    date = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        app_label = 'cosinnus_notifications'
+        unique_together = (('user', 'notification_id', 'group'),)
+        verbose_name = _('Notification preference')
+        verbose_name_plural = _('Notification preferences')
+
+    def __str__(self):
+        return "<User notification preference: %(user)s, group: %(group)s, notification_id: %(notification_id)s, is_active: %(is_active)d>" % {
+            'user': self.user,
+            'notification_id': self.notification_id,
+            'is_active': self.is_active,
+            'group': self.group,
+        }
 
 
 import django
