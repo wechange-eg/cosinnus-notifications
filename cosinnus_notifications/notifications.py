15ab6e9260685e86ab9b670bb0a303e6cd3c6235
Sascha Narr <saschanarr@googlemail.com>
2015-03-17 13:56:15 +0100

    Reworked notifications to run in a Thread for each notification type fired

--- cosinnus_notifications/notifications.py
+++ cosinnus_notifications/notifications.py
@@ -3,21 +3,21 @@ from __future__ import unicode_literals
 
 import logging
 
-from collections import defaultdict
-
-from django.template.loader import render_to_string
+from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.utils.importlib import import_module
+from django.template.loader import render_to_string
 
 from cosinnus.core.mail import get_common_mail_context, send_mail_or_fail
 from cosinnus.core.registries.apps import app_registry
 from cosinnus.models.group import CosinnusGroup
 from cosinnus.models.tagged import BaseTaggableObjectModel
 from cosinnus_notifications.models import UserNotificationPreference
-from cosinnus.utils.functions import ensure_dict_keys
 from cosinnus.templatetags.cosinnus_tags import full_name
-from django.core.urlresolvers import reverse
-from django.core.exceptions import ImproperlyConfigured
+from cosinnus.utils.functions import ensure_dict_keys
+from threading import Thread
+
 
 logger = logging.getLogger('cosinnus')
 
@@ -41,6 +41,8 @@ notifications = {
     },  
 }
 
+
+
 def _find_notification(signal):
     """ Finds a configured notification for a received signal """
     for signal_id, options in notifications.items():
@@ -48,16 +50,6 @@ def _find_notification(signal):
             return signal_id
     return None
 
-def is_notification_active(notification_id, user, group):
-    """ Checks against the DB if a user notifcation preference exists, and if so, if it is set to active """
-    try:
-        preference = UserNotificationPreference.objects.get(user=user, group=group, notification_id=notification_id)
-    except UserNotificationPreference.DoesNotExist:
-        # if not set in DB, check if preference is default on 
-        if notification_id in notifications and notifications[notification_id].get('default', False):
-            return True
-        return False
-    return preference.is_active
 
 def set_user_group_notifications_special(user, group, all_or_none_or_custom):
     """ Sets the user preference settings for a group to all or none or custom (deleting the special setting flag) """
@@ -87,98 +79,6 @@ def set_user_group_notifications_special(user, group, all_or_none_or_custom):
         if all_or_none_or_custom == "none":
             UserNotificationPreference.objects.create(user=user, group=group, notification_id=NO_NOTIFICATIONS_ID, is_active=True)
         
-        
-def check_user_wants_notification(user, notification_id, obj):
-    """ Do multiple pre-checks and a DB check to find if the user wants to receive a mail for a 
-        notification event. """
-    group = None
-    
-    if type(obj) is CosinnusGroup or issubclass(obj.__class__, CosinnusGroup):
-        group = obj
-    elif issubclass(obj.__class__, BaseTaggableObjectModel):
-        group = obj.group
-    elif hasattr(obj, 'group'):
-        group = obj.group
-    else:
-        raise ImproperlyConfigured('A signal for a notification was received, but the supplied object\'s group could not be determined. \
-            If your object is not a CosinnusGroup or a BaseTaggableObject, you can fix this by patching a ``group`` attribute onto it.')
-    user_in_group = group.is_member(user)
-    
-    # print ">> checking if user wants notification ", notification_id, "(is he in the group/object's group?)", user_in_group
-    if not user_in_group:
-        # >>> user didn't want notification or there was no group
-        return False
-    if is_notification_active(NO_NOTIFICATIONS_ID, user, group):
-        # >>> user didn't want notification because he wants none ever!
-        return False
-    if is_notification_active(ALL_NOTIFICATIONS_ID, user, group):
-        # >>> user wants notification because he wants all!
-        return True
-    ret = is_notification_active(notification_id, user, group)
-    # >> checked his settings, and user wants this notification is", ret
-    return ret
-
-def notification_receiver(sender, user, obj, audience, **kwargs):
-    """ Generic receiver function for all notifications 
-        sender: the main object that is being updated / created
-        user: the user that modified the object and caused the event
-        audience: a list of users that are potential targets for the event 
-    """
-    from cosinnus.utils.context_processors import cosinnus as cosinnus_context
-    signal = kwargs['signal']
-    # find out configured signal id for the signal we received
-    notification_id = _find_notification(signal)
-    if not notification_id:
-        return
-    options = notifications[notification_id]
-    
-    # update / overwrite options with extra kwargs sent by the signalling code piece individually
-    if 'extra' in kwargs: 
-        # deepcopy does not work here, so create a new dict
-        copy_options = {}
-        copy_options.update(options)
-        copy_options.update(kwargs['extra'])
-        options = copy_options
-    
-    for receiver in audience:
-        if check_user_wants_notification(receiver, notification_id, obj):
-            template = options['mail_template']
-            subj_template = options['subject_template']
-            if sender.request:
-                context = get_common_mail_context(sender.request)
-                context.update(cosinnus_context(sender.request))
-            else:
-                #print ">>> warn: no request in sender"
-                context = {}
-            
-            context.update({
-                'receiver': receiver,
-                'receiver_name': full_name(receiver),
-                'sender': user,
-                'sender_name': full_name(user),
-                'object': obj,
-                'notification_settings_url': '%s%s' % (context['domain_url'], reverse('cosinnus:notifications')),
-            })
-            # additional context for BaseTaggableObjectModels
-            if issubclass(obj.__class__, BaseTaggableObjectModel):
-                context.update({
-                    'object_name': obj.title,
-                    'group_name': obj.group.name,
-                })
-            else:
-                group = getattr(obj, 'group', None)
-                context.update({
-                    'group_name': getattr(group, 'name', '<notfound>'),
-                })
-            try:
-                context.update({
-                    'object_url': obj.get_absolute_url(),
-                })
-            except:
-                pass
-                
-            subject = render_to_string(subj_template, context)
-            send_mail_or_fail(receiver.email, subject, template, context)
 
 
 def init_notifications():
@@ -208,4 +108,111 @@ def init_notifications():
                 for signal in options['signals']:
                     signal.connect(notification_receiver)
     logger.info('Cosinnus_notifications: init complete. Available notification signals: %s' % notifications.keys())
+
+
+class NotificationsThread(Thread):
+
+    def __init__(self, sender, user, obj, audience, notification_id, options):
+        super(NotificationsThread, self).__init__()
+        self.sender = sender
+        self.user = user
+        self.obj = obj
+        self.audience = audience
+        self.notification_id = notification_id
+        self.options = options
+    
+    def is_notification_active(self, notification_id, user, group):
+        """ Checks against the DB if a user notifcation preference exists, and if so, if it is set to active """
+        try:
+            preference = UserNotificationPreference.objects.get(user=user, group=group, notification_id=notification_id)
+        except UserNotificationPreference.DoesNotExist:
+            # if not set in DB, check if preference is default on 
+            if notification_id in notifications and notifications[notification_id].get('default', False):
+                return True
+            return False
+        return preference.is_active
+    
+    def check_user_wants_notification(self, user, notification_id, obj):
+        """ Do multiple pre-checks and a DB check to find if the user wants to receive a mail for a 
+            notification event. """
+        group = None
+        
+        if type(obj) is CosinnusGroup or issubclass(obj.__class__, CosinnusGroup):
+            group = obj
+        elif issubclass(obj.__class__, BaseTaggableObjectModel):
+            group = obj.group
+        elif hasattr(obj, 'group'):
+            group = obj.group
+        else:
+            raise ImproperlyConfigured('A signal for a notification was received, but the supplied object\'s group could not be determined. \
+                If your object is not a CosinnusGroup or a BaseTaggableObject, you can fix this by patching a ``group`` attribute onto it.')
+        user_in_group = group.is_member(user)
+        
+        # print ">> checking if user wants notification ", notification_id, "(is he in the group/object's group?)", user_in_group
+        if not user_in_group:
+            # >>> user didn't want notification or there was no group
+            return False
+        if self.is_notification_active(NO_NOTIFICATIONS_ID, user, group):
+            # >>> user didn't want notification because he wants none ever!
+            return False
+        if self.is_notification_active(ALL_NOTIFICATIONS_ID, user, group):
+            # >>> user wants notification because he wants all!
+            return True
+        ret = self.is_notification_active(notification_id, user, group)
+        # >> checked his settings, and user wants this notification is", ret
+        return ret
+
+
+    def run(self):
+        from cosinnus.utils.context_processors import cosinnus as cosinnus_context
+        
+        for receiver in self.audience:
+            if self.check_user_wants_notification(receiver, self.notification_id, self.obj):
+                template = self.options['mail_template']
+                subj_template = self.options['subject_template']
+                if self.sender.request:
+                    context = get_common_mail_context(self.sender.request)
+                    context.update(cosinnus_context(self.sender.request))
+                else:
+                    context = {} #print ">>> warn: no request in sender"
+                context.update({'receiver':receiver, 'receiver_name':full_name(receiver), 'sender':self.user, 'sender_name':full_name(self.user), 'object':self.obj, 'notification_settings_url':'%s%s' % (context['domain_url'], reverse('cosinnus:notifications'))})
+                # additional context for BaseTaggableObjectModels
+                if issubclass(self.obj.__class__, BaseTaggableObjectModel):
+                    context.update({'object_name':self.obj.title, 'group_name':self.obj.group.name})
+                else:
+                    group = getattr(self.obj, 'group', None)
+                    context.update({'group_name':getattr(group, 'name', '<notfound>')})
+                try:
+                    context.update({'object_url':self.obj.get_absolute_url()})
+                except:
+                    print "pass"
+                subject = render_to_string(subj_template, context)
+                send_mail_or_fail(receiver.email, subject, template, context)
+                
+        return
+    
+
+def notification_receiver(sender, user, obj, audience, **kwargs):
+    """ Generic receiver function for all notifications 
+        sender: the main object that is being updated / created
+        user: the user that modified the object and caused the event
+        audience: a list of users that are potential targets for the event 
+    """
+    signal = kwargs['signal']
+    # find out configured signal id for the signal we received
+    notification_id = _find_notification(signal)
+    if not notification_id:
+        return
+    options = notifications[notification_id]
+    
+    # update / overwrite options with extra kwargs sent by the signalling code piece individually
+    if 'extra' in kwargs: 
+        # deepcopy does not work here, so create a new dict
+        copy_options = {}
+        copy_options.update(options)
+        copy_options.update(kwargs['extra'])
+        options = copy_options
+    
+    notification_thread = NotificationsThread(sender, user, obj, audience, notification_id, options)
+    notification_thread.start()
 
