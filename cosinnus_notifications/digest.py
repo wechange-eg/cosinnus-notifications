0d6a869eb09371c760b2ba1531ed72e8929ec37a
Sascha Narr <saschanarr@googlemail.com>
2016-08-23 14:48:00 +0200

    Now reading/saving the last digest dates from CosinnusPortalsaved_infos. Added exception handling.

--- cosinnus_notifications/digest.py
+++ cosinnus_notifications/digest.py
@@ -4,7 +4,12 @@ from __future__ import unicode_literals
 from datetime import timedelta
 import logging
 
+from django.contrib.auth import get_user_model
+from django.core.urlresolvers import reverse
+from django.db.models import Q
 from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.utils.encoding import force_text
 from django.utils import translation
 from django.utils.safestring import mark_safe
 from django.utils.timezone import now
@@ -15,12 +20,10 @@ from cosinnus_notifications.models import UserNotificationPreference,\
     NotificationEvent
 from cosinnus_notifications.notifications import NO_NOTIFICATIONS_ID,\
     ALL_NOTIFICATIONS_ID
-from django.core.urlresolvers import reverse
-from django.utils.html import strip_tags
 from cosinnus.templatetags.cosinnus_tags import full_name, cosinnus_setting
 from cosinnus.core.mail import send_mail_or_fail
-from django.contrib.auth import get_user_model
 from cosinnus.utils.permissions import check_object_read_access
+import traceback
 
 logger = logging.getLogger('cosinnus')
 
@@ -40,91 +43,107 @@ def send_digest_for_current_portal(digest_setting):
     portal = CosinnusPortal.get_current()
     portal_group_ids = portal.groups.all().filter(is_active=True).values_list('id', flat=True)
     
-    # TODO: get this from a (CosinusPortal?) DB field
-    TIME_DIGEST_START = None
+    # read the time for the last sent digest of this time
+    # (its saved as a string, but django QS will auto-box it when filtering on datetime fields)
+    TIME_DIGEST_START = portal.saved_infos.get(CosinnusPortal.SAVED_INFO_LAST_DIGEST_SENT % digest_setting, None)
     if not TIME_DIGEST_START:
         TIME_DIGEST_START = now() - timedelta(days=UserNotificationPreference.SETTINGS_DAYS_DURATIONS[digest_setting])
-    # TODO: save this to the DB now or after the digest finished
     TIME_DIGEST_END = now()
     
     # the main Notification Events QS. anything not in here did not happen in the digest's time span
     timescope_notification_events = NotificationEvent.objects.filter(date__gte=TIME_DIGEST_START, date__lt=TIME_DIGEST_END)
-    
     users = get_user_model().objects.all().filter(id__in=portal.members)
+    
+    logger.info('Now starting to sending out digests of SETTING=%s in Portal "%s". Data in extra.' % \
+                (UserNotificationPreference.SETTING_CHOICES[digest_setting][1], portal.slug), extra={
+        'notification_event_count': timescope_notification_events.count(),
+        'potential_user_count': users.count(), 
+    })
+    
     emailed = 0
     for user in users:
-        # only active users that have logged in before accepted the TOS get notifications
-        if not user.is_active or not user.last_login or not cosinnus_setting(user, 'tos_accepted'):
-            continue
-        
-        # find out any notification preferences the user has for groups in this portal with the daily/weekly setting
-        # if he doesn't have any, we will not send a mail for them
-        prefs = UserNotificationPreference.objects.filter(user=user, group_id__in=portal_group_ids, setting=digest_setting)
-        prefs = prefs.exclude(notification_id=NO_NOTIFICATIONS_ID)
-        
-        if len(prefs) == 0:
-            continue
-        
-        # only for these groups does the user get any digest news at all
-        pref_group_ids = list(set([pref.group for pref in prefs]))
-        # get all notification events for these groups
-        events = timescope_notification_events.filter(group_id__in=pref_group_ids)
-        # and also only those where the user is in the intended audience
-        events = events.filter(audience__contains=',%d,' % user.id)
-        
-        if events.count() == 0:
-            continue
-        
-        # collect a comparable hash for all wanted user prefs
-        wanted_group_notifications = ['%(group_id)d__%(notification_id)s' % {
-            'group_id': pref.group_id,
-            'notification_id': pref.notification_id,
-        } for pref in prefs]
-        
-        # cluster event messages by group. from here on, the user will definitely get an email.
-        body_html = ''
-        for group in list(set(events.values_list('group', flat=True))): # FIXME: is values-list unique?
-            group_events = events.filter(group=group).order_by('notification_id')
+        try:
+            # only active users that have logged in before accepted the TOS get notifications
+            if not user.is_active or not user.last_login or not cosinnus_setting(user, 'tos_accepted'):
+                continue
             
+            # find out any notification preferences the user has for groups in this portal with the daily/weekly setting
+            # if he doesn't have any, we will not send a mail for them
+            prefs = UserNotificationPreference.objects.filter(user=user, group_id__in=portal_group_ids, setting=digest_setting)
+            prefs = prefs.exclude(notification_id=NO_NOTIFICATIONS_ID)
             
-            # filter only those events that the user actually has in his prefs, for this group and also
-            # check for target object existing, being visible to user, and other sanity checks if the user should see this object
-            wanted_group_events = []
-            for event in group_events:
-                if user == event.user:
-                    continue  # users don't receive infos about events they caused
-                if not (('%d__%s' % (event.group_id, ALL_NOTIFICATIONS_ID) in wanted_group_notifications) or \
-                        ('%d__%s' % (event.group_id, event.notification_id) in wanted_group_notifications)):
-                    continue  # must have an actual subscription to that event type
-                if event.target_object is None:
-                    continue  # referenced object has been deleted by now
-                if not check_object_read_access(event.target_object, user):
-                    continue  # user must be able to even see referenced object 
-                wanted_group_events.append(event)
+            if len(prefs) == 0:
+                continue
+            
+            # only for these groups does the user get any digest news at all
+            pref_group_ids = list(set([pref.group for pref in prefs]))
+            # get all notification events for these groups
+            events = timescope_notification_events.filter(group_id__in=pref_group_ids)
+            # and also only those where the user is in the intended audience
+            events = events.filter(audience__contains=',%d,' % user.id)
+            
+            if events.count() == 0:
+                continue
+            
+            # collect a comparable hash for all wanted user prefs
+            wanted_group_notifications = ['%(group_id)d__%(notification_id)s' % {
+                'group_id': pref.group_id,
+                'notification_id': pref.notification_id,
+            } for pref in prefs]
+            
+            # cluster event messages by group. from here on, the user will definitely get an email.
+            body_html = ''
+            for group in list(set(events.values_list('group', flat=True))): 
+                group_events = events.filter(group=group).order_by('notification_id')
                 
+                # filter only those events that the user actually has in his prefs, for this group and also
+                # check for target object existing, being visible to user, and other sanity checks if the user should see this object
+                wanted_group_events = []
+                for event in group_events:
+                    if user == event.user:
+                        continue  # users don't receive infos about events they caused
+                    if not (('%d__%s' % (event.group_id, ALL_NOTIFICATIONS_ID) in wanted_group_notifications) or \
+                            ('%d__%s' % (event.group_id, event.notification_id) in wanted_group_notifications)):
+                        continue  # must have an actual subscription to that event type
+                    if event.target_object is None:
+                        continue  # referenced object has been deleted by now
+                    if not check_object_read_access(event.target_object, user):
+                        continue  # user must be able to even see referenced object 
+                    wanted_group_events.append(event)
+                    
+                if wanted_group_events:
+                    group_html = '\n'.join([render_digest_item_for_notification_event(event, user) for event in wanted_group_events])
+                    group_template_context = {
+                        'group_html': mark_safe(group_html),
+                        'group': group,
+                    }
+                    # TODO: create a group-layout template with group header and a fill-in for the single 
+                    #body_html += render_to_string('cosinnus/path/to/body/template', context=group_template_context) + '\n'
+                    body_html += group_html + '\n'
             
-            group_html = '\n'.join([render_digest_item_for_notification_event(event, user) for event in wanted_group_events])
-            group_template_context = {
-                'group_html': mark_safe(group_html),
-                'group': group,
-            }
-            # TODO: create a group-layout template with group header and a fill-in for the single 
-            #body_html += render_to_string('cosinnus/path/to/body/template', context=group_template_context) + '\n'
-            body_html += group_html + '\n'
-        
-        # send actual email with full frame template
-        _send_digest_email(user, mark_safe(body_html), TIME_DIGEST_END, digest_setting)
-        emailed += 1
+            # send actual email with full frame template
+            _send_digest_email(user, mark_safe(body_html), TIME_DIGEST_END, digest_setting)
+            emailed += 1
+            
+        except Exception, e:
+            # we never want this subroutine to just die, we need the final saves at the end to ensure
+            # the same items do not get into digests twice
+            logger.error('An error occured while doing a digest for a user! Exception was: %s' % force_text(e), 
+                         extra={'exception': e, 'trace': traceback.format_exc(), 'user_mail': user.email, 'digest_setting': digest_setting})
+            
+    # save the end time of the digest period as last digest time for this type
+    portal.saved_infos[CosinnusPortal.SAVED_INFO_LAST_DIGEST_SENT % digest_setting] = TIME_DIGEST_END
+    portal.save()
     
     deleted = cleanup_stale_notifications()
     
     extra_log = {
         'users_emailed': emailed,
         'total_users': len(users),
-        'delted_stale_notifications': deleted,
-        'fresh_notifications_alive': NotificationEvent.objects.all().count(),
+        'deleted_stale_notifications': deleted,
+        'remaining_past_and_future_notifications': NotificationEvent.objects.all().count(),
     }
-    logger.info('Finished sending out digests of SETTING=%s. Data in extra.' % UserNotificationPreference.SETTING_CHOICES[digest_setting][1], extra=extra_log)
+    logger.info('Finished sending out digests of SETTING=%s in Portal "%s". Data in extra.' % (UserNotificationPreference.SETTING_CHOICES[digest_setting][1], portal.slug), extra=extra_log)
     if settings.DEBUG:
         print extra_log
     
@@ -150,7 +169,6 @@ def _send_digest_email(receiver, body_html, digest_generation_time, digest_setti
     try:
         translation.activate(getattr(receiver.cosinnus_profile, 'language', settings.LANGUAGES[0][0]))
         
-        # TODO: FIXME:
         print ">> now sending HTML to user", receiver.get_full_name()
         print body_html
         return
