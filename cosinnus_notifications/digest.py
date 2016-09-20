8a01c5229e554c6d40667b9bf9725c833da82c54
Sascha Narr <saschanarr@googlemail.com>
2016-09-20 15:44:58 +0200

    Max notification events now set to: 2 * maximum_digest_duration + 1 days

--- cosinnus_notifications/digest.py
+++ cosinnus_notifications/digest.py
@@ -310,7 +310,7 @@ def cleanup_stale_notifications():
         @return the count of the items deleted """
         
     max_days = max(dict(UserNotificationPreference.SETTINGS_DAYS_DURATIONS).values())
-    time_digest_stale = now() - datetime.timedelta(days=max_days*3)
+    time_digest_stale = now() - datetime.timedelta(days=(1+max_days*2))
     stale_notification_events = NotificationEvent.objects.filter(date__lt=time_digest_stale)
     
     deleted = stale_notification_events.count()
