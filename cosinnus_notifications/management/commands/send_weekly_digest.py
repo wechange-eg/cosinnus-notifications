c2e627569fcc3d3538826a67fedf7cbb4d786052
Sascha Narr <saschanarr@googlemail.com>
2016-09-20 16:39:58 +0200

    Add outer Exception logging for digest management command

--- cosinnus_notifications/management/commands/send_weekly_digest.py
+++ cosinnus_notifications/management/commands/send_weekly_digest.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import logging
+import traceback
+
 from django.core.management.base import BaseCommand, CommandError
 from cosinnus_notifications.digest import send_digest_for_current_portal
 from cosinnus_notifications.models import UserNotificationPreference
 from cosinnus.core.middleware import initialize_cosinnus_after_startup
+from django.utils.encoding import force_text
+
+logger = logging.getLogger('cosinnus')
+
 
 class Command(BaseCommand):
     help = 'Closes the specified poll for voting'
 
     def handle(self, *args, **options):
-        initialize_cosinnus_after_startup()
-        send_digest_for_current_portal(UserNotificationPreference.SETTING_WEEKLY)
+        try:
+            initialize_cosinnus_after_startup()
+            send_digest_for_current_portal(UserNotificationPreference.SETTING_WEEKLY)
+        except Exception, e:
+            logger.error('An critical error occured during weekly digest generation and bubbled up completely! Exception was: %s' % force_text(e),
+                         extra={'exception': e, 'trace': traceback.format_exc()})
+                                
