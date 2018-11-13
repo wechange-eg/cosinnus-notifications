9e7afcfb60642e4b3c49755fef0905f66667d115
sascha <sn@sinnwerkstatt.com>
2018-11-13 16:08:47 +0100

    Django 2.0 compatibility

--- cosinnus_notifications/urls.py
+++ cosinnus_notifications/urls.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.conf.urls import url
 from cosinnus_notifications import views
 
+app_name = 'notifications'
+
 cosinnus_root_patterns = [
     url(r'^profile/notifications/$', views.notification_preference_view, name='notifications'),
     url(r'^profile/reset_notifications/$', views.notification_reset_view, name='reset-notifications'),           
